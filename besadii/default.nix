c01a45eb56bc8a398a78b318f2f43496bb210ee7
Vincent Ambo <mail@tazj.in>
2020-06-26 22:22:01 +0100

    feat(besadii): Trigger separate builds for build target sets
    In CL/570 we split up the build targets into different buckets, with
    the idea that this should help us avoid the disk space issues on
    Sourcehut.
    
    This commit changes Besadii to read the list of target sets from a
    file and trigger a separate build for each one of them.
    
    Change-Id: If280fda5f40cd130c534c40911072e47c2d8f2be
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/608
    Reviewed-by: lukegb <lukegb@tvl.fyi>

--- besadii/default.nix
+++ besadii/default.nix
@@ -1,8 +1,14 @@
 # This program is used as a git post-update hook to trigger builds on
 # sourcehut.
-{ depot, ... }:
+{ ciBuilds, depot, ... }:
 
-depot.nix.buildTypedGo.program {
+let
+  inherit (builtins) toFile toJSON;
+in depot.nix.buildTypedGo.program {
   name = "besadii";
   srcs = [ ./main.go2 ];
+
+  x_defs = {
+    "main.TargetList" = toFile "ci-targets.json" (toJSON ciBuilds.__evaluatable);
+  };
 }

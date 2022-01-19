5612410d629276a68c26878fbc934b8e70d55491
Vincent Ambo <mail@tazj.in>
2022-01-19 18:01:41 +0300

    refactor(nix/buildkite): Move fetch-parent-targets script here
    This is no longer TVL-specific and should live here with the other
    generalised stuff.
    
    Change-Id: I95a1b4c0321f34812162d6fd40568269abf639dd
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/5006
    Tested-by: BuildkiteCI
    Autosubmit: tazjin <tazjin@tvl.su>
    Reviewed-by: ezemtsov <eugene.zemtsov@gmail.com>

---
+++ buildkite/fetch-parent-targets.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+
+# Each Buildkite build stores the derivation target map as a pipeline
+# artifact. This script determines the most appropriate commit (the
+# fork point of the current chain from HEAD) and fetches the artifact.
+#
+# New builds can be based on HEAD before the pipeline for the last
+# commit has finished, in which case it is possible that the fork
+# point has no derivation map. To account for this, up to 3 commits
+# prior to HEAD are also queried to find a map.
+#
+# If no map is found, the failure mode is not critical: We simply
+# build all targets.
+
+: ${DRVMAP_PATH:=pipeline/drvmap.json}
+
+function most_relevant_builds {
+    git fetch -v origin "${BUILDKITE_PIPELINE_DEFAULT_BRANCH}"
+    local FIRST=$(git merge-base --fork-point HEAD FETCH_HEAD)
+    local SECOND=$(git rev-parse "$FIRST~1")
+    local THIRD=$(git rev-parse "$FIRST~2")
+
+    curl 'https://graphql.buildkite.com/v1' \
+         --silent \
+         -H "Authorization: Bearer $(cat /run/agenix/buildkite-graphql-token)" \
+         -d "{\"query\": \"query { pipeline(slug: \\\"$BUILDKITE_ORGANIZATION_SLUG/$BUILDKITE_PIPELINE_SLUG\\\") { builds(commit: [\\\"$FIRST\\\",\\\"$SECOND\\\",\\\"$THIRD\\\"]) { edges { node { uuid }}}}}\"}" | \
+         jq -r '.data.pipeline.builds.edges[] | .node.uuid'
+}
+
+mkdir -p tmp
+for build in $(most_relevant_builds); do
+    echo "Checking artifacts for build $build"
+    buildkite-agent artifact download --build "${build}" "${DRVMAP_PATH}" 'tmp/' || true
+
+    if [[ -f "tmp/${DRVMAP_PATH}" ]]; then
+        echo "Fetched target map from build ${build}"
+        mv "tmp/${DRVMAP_PATH}" tmp/parent-target-map.json
+        break
+    fi
+done

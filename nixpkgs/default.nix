3557885b071c1e3d814fca8cf52f0fb30c483076
sterni <sternenseemann@systemli.org>
2025-11-16 12:37:26 +0100

    subtree(3p/cppnix): import 2.3-maintenance branch from tvlfyi/nix
    Dèjá vu…
    
    Change-Id: I29fd79a4decf0ec57cb63543ece988fcd295bd3e

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -35,7 +35,7 @@ let
         checkMeta = false;
         permittedInsecurePackages = [
           # TODO(tazjin): https://github.com/tvlfyi/nix/pull/5
-          "nix-2.3.18-4f415e6"
+          "nix-2.3.18-canon"
         ];
       };
 

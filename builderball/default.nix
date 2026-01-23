728cd1129f1c113ae7f4155f522854cdf99ab77f
Evgeny Zemtsov <eugene.zemtsov@gmail.com>
2026-01-23 15:57:01 +0100

    refactor(gitignore): replace gitignoreSource with pkgs.nix-gitignore
    Using native nix-gitignore has certain advantages over custom
    gitignoreSource:
    
      1. It removes dependency on depot for some build processes.
      2. It is more explicit, as it doesn't magically depend on files outside
      of the build scope.
      3. It allows adding extra filters in nix builds which are not in
      .gitignore file.
    
    This change was initially triggered by the desire to export builderball
    in tvlkit, which doesn't have gitignoreSource. This commit extends that
    migration repo-wide and removes the custom third_party/gitignoreSource
    wrapper along with its external gitignore.nix dependency.
    
    Change-Id: Ib805b7fbb82905613c41721fa0c51ca65f34963f
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/13978
    Reviewed-by: tazjin <tazjin@tvl.su>
    Tested-by: BuildkiteCI

--- builderball/default.nix
+++ builderball/default.nix
@@ -1,8 +1,8 @@
-{ depot, pkgs, ... }:
+{ pkgs, ... }:
 
 pkgs.buildGoModule {
   name = "builderball";
-  src = depot.third_party.gitignoreSource ./.;
+  src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
   vendorHash = "sha256:1prdkm05bdbyinwwgrbwl8pazayg5cp61dlkmygxwbp880zxpqfm";
   meta.description = "Nix cache proxy which forwards clients to the first available cache";
 }

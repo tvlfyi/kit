50519c1923f64f0bb7bafc7c265ffe66c2064e82
sterni <sternenseemann@systemli.org>
2026-03-06 17:44:48 +0100

    test(nix/dependency-analyzer): add sanity check that deps are detected
    We had, at some point, a bug where dependency-analyzer wouldn't detect
    any dependencies for some versions of Nix. This test case catches such a
    situation (though it is not very sophisticated).
    
    Change-Id: I061ec1e3062f641dd34ea178b3018b2ae03bdd88
    Reviewed-on: https://cl.tvl.fyi/c/depot/+/14030
    Reviewed-by: sterni <sternenseemann@systemli.org>
    Autosubmit: sterni <sternenseemann@systemli.org>
    Tested-by: BuildkiteCI

--- dependency-analyzer/tests/default.nix
+++ dependency-analyzer/tests/default.nix
@@ -15,7 +15,8 @@ let
   knownDrvs = drvsToPaths (
     builtins.filter lib.isDerivation (builtins.attrValues depot.third_party.lisp)
   );
-  exampleMap = plainDrvDepMap knownDrvs;
+  examplePlainMap = plainDrvDepMap knownDrvs;
+  exampleKnownMap = depot.nix.dependency-analyzer knownDrvs;
 
   # These will be needed to index into the attribute set which can't have context
   # in the attribute names.
@@ -23,14 +24,25 @@ let
 in
 
 runTestsuite "dependency-analyzer" [
-  (it "checks plainDrvDepMap properties" [
+  (it "produces well-formed plainDrvDepMaps" [
     (assertEq "all known drvs are marked known"
-      (builtins.all (drv: exampleMap.${drv}.known) knownDrvsNoContext)
+      (builtins.all (drv: examplePlainMap.${drv}.known) knownDrvsNoContext)
       true)
     (assertEq "no unknown drv is marked known"
       (builtins.all (entry: !entry.known) (
-        builtins.attrValues (builtins.removeAttrs exampleMap knownDrvsNoContext)
+        builtins.attrValues (builtins.removeAttrs examplePlainMap knownDrvsNoContext)
       ))
       true)
   ])
+  (
+    let
+      drvPath = d: builtins.unsafeDiscardStringContext d.drvPath;
+      inherit (depot.third_party.lisp) easy-routes hunchentoot;
+    in
+    it "detects dependencies" [
+      (assertEq "easy-routes depends on hunchentoot"
+        (builtins.elem (drvPath hunchentoot) exampleKnownMap.${drvPath easy-routes}.knownDeps)
+        true)
+    ]
+  )
 ]
